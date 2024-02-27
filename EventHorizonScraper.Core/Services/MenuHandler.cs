a653714443d6c501dd59531a0ea461f430ea9419
jordennorton <jordennorton@hotmail.co.uk>
2024-02-27 18:06:19 +0000

    Create Menu Handler class

---
+++ EventHorizonScraper.Core/Services/MenuHandler.cs
@@ -0,0 +1,45 @@
+using NLog;
+
+namespace EventHorizonScraper.Core.Services;
+
+public class MenuHandler
+{
+    public void DisplayMenu()
+    {
+        var logger = LogManager.GetCurrentClassLogger();
+        var continueRunning = true;
+            
+        while (continueRunning)
+        {
+            try
+            {
+                Console.WriteLine("Ready to start scraping? \n Y or N");
+                string? startScraping = Console.ReadLine()?.ToLower();
+                switch (startScraping)
+                {
+                    case "y":
+                        logger.Info("Scraping: initiated");
+                        Console.WriteLine("Scraping...");
+                        Console.ReadLine();
+                        break;
+                        
+                    case "n":
+                        logger.Info("Quit: startScraping - User entered 'n'");
+                        Console.WriteLine("OK, come back when you are ready!");
+                        continueRunning = false;
+                        break;
+                        
+                    default:
+                        logger.Info($"Invalid input: startScraping - user entered '{startScraping}'");
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "An unexpected error occurred.");
+                continueRunning = false;
+            }
+        }
+    }
+}
