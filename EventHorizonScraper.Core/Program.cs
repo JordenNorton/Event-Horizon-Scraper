b9512d30080d361aed5797ad0f4f24b5d3623d4b
jordennorton <jordennorton@hotmail.co.uk>
2024-02-27 18:06:47 +0000

    Move menu logic to 'Menu Handler'

--- EventHorizonScraper.Core/Program.cs
+++ EventHorizonScraper.Core/Program.cs
@@ -1,24 +1,20 @@
-﻿using NLog;
+﻿using EventHorizonScraper.Core.Services;
+using NLog;
 
 namespace EventHorizonScraper.Core
 {
     public static class Program
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-
+        private static readonly MenuHandler MenuHandler = new MenuHandler();
+        
         static void Main(string[] args)
         {
+            
             Logger.Info("Application is starting...");
-
-            try
-            {
-                
-            }
-            catch (Exception e)
-            {
-                Logger.Error(e, "An unexpected error occurred.");
-            }
-
+            
+            MenuHandler.DisplayMenu();
+            
             Logger.Warn("Application is ending...");
         }
     }
