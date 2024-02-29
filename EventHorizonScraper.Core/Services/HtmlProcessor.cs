836cad2c6a12453361972de4002c164308fed28f
jordennorton <jordennorton@hotmail.co.uk>
2024-02-29 15:21:59 +0000

    Extract event data & append to list<EventCard>

--- EventHorizonScraper.Core/Services/HtmlProcessor.cs
+++ EventHorizonScraper.Core/Services/HtmlProcessor.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using System.Text.Json;
+using EventHorizonScraper.Core.Models;
 using HtmlAgilityPack;
 using NLog;
 
@@ -15,55 +16,85 @@ public class HtmlProcessor
         var htmlDoc = new HtmlDocument();
         htmlDoc.LoadHtml(content);
 
-        List<string> titles = new List<string>();
+        //Initialise a list of type "EventCard" (Model)
+        List<EventCard> eventCards = new List<EventCard>();
+        
+        //Set event card limit
         int cardCount = 5;
         
         for (int i = 1; i <= cardCount; i++)
         {
-            // Construct the XPath query dynamically to iterate through cards
-            string xpath = $"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/div/div/section[1]/div/section/div/div/section/ul/li[{i}]/div/div[2]/section/div/section[2]/div/a/h2";;
+            //XPath strings
+            string cardLinkXPath = $"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/div/div/section[1]/div/section/div/div/section/ul/li[{i}]/div/div[2]/section/div/section[2]/div/a";
+            string dateXPath =
+                $"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/div/div/section[1]/div/section/div/div/section/ul/li[{i}]/div/div[2]/section/div/section[2]/div/p[1]";
+            string organiserXPath = $"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/div/div/section[1]/div/section/div/div/section/ul/li[{i}]/div/div[2]/section/div/section[2]/div/p[2]";
 
-            // Select nodes based on the constructed XPath
-            var eventTitles = htmlDoc.DocumentNode.SelectNodes(xpath);
+            //XPath nodes
+            var eventTitles = htmlDoc.DocumentNode.SelectNodes(cardLinkXPath); //Gets event title & location (from html tag 'data-event-location')
+            var eventDates = htmlDoc.DocumentNode.SelectSingleNode(dateXPath);
+            var eventOrganisers = htmlDoc.DocumentNode.SelectSingleNode(organiserXPath);
             
             if (eventTitles != null)
             {
-                foreach (var eventTitleNode in eventTitles)
+                try
                 {
-                    StringBuilder sb = new StringBuilder();
-                    string eventTitleText = eventTitleNode.InnerText.Trim();
-        
-                    for (int e = 0; e < eventTitleText.Length; e++)
+                    foreach (var eventTitleNode in eventTitles)
                     {
-                        char c = eventTitleText[e];
-            
-                        if (c == ' ' || (c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_' || c == '-' || c == '&')
+                        //Clean title of unwanted characters
+                        string cleanedTitle = CleanText(eventTitleNode.InnerText.Trim());
+                        
+                        //Assign manipulated event values to strings
+                        string eventDate = eventDates.InnerText.Trim();
+                        string eventLocation = eventTitleNode.Attributes["data-event-location"].Value;
+                        string eventOrganiser = eventOrganisers.InnerText.Trim();
+                        
+                        //Set values using EventCard model
+                        EventCard eventCard = new EventCard
                         {
-                            sb.Append(c);
-                        }
+                            Title = cleanedTitle,
+                            Date = eventDate,
+                            Location = eventLocation,
+                            Organiser = eventOrganiser
+                        };
+                        
+                        //append events to the eventCards list
+                        eventCards.Add(eventCard);
                     }
-                    
-                    string cleanedTitle = sb.ToString();
-                    cleanedTitle = cleanedTitle.Replace("&amp", "&");
-                    
-                    titles.Add(cleanedTitle);
+
+                    Logger.Info("Html extracted succesfully");
+                }
+                catch (Exception e)
+                {
+                    Logger.Error($"Failed to extract html data: {e.Message}");
                 }
             }
         }
         
-        Logger.Info("Serializing!");
-        string jsonObject = JsonSerializer.Serialize(titles);
-        Logger.Info("Finished serializing");
-        
-        Console.WriteLine(jsonObject);
+        //Allowed characters method - Cleans the event title text of unwanted characters
+        string CleanText(string input)
+        {
+            var allowedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz._-& ";
+            
+            //returns the cleaned title & changes any escaped &amp's to the & charachter
+            return new string(input.Where(c => allowedChars.Contains(c)).ToArray()).Replace("&amp","&");
+        }
 
         
+        /* - Testing Start - */
+        
+        Logger.Info("Serializing!");
+        string jsonObject = JsonSerializer.Serialize(eventCards);
+        Logger.Info("Finished serializing");
+        
         //List output checker!
-        List<string> jsonOutput = JsonSerializer.Deserialize<List<string>>(jsonObject);
-        foreach (string title in jsonOutput)
+        List<EventCard> jsonOutput = JsonSerializer.Deserialize<List<EventCard>>(jsonObject);
+        foreach (EventCard eventCard in jsonOutput)
         {
-            Console.WriteLine(title);           
+            Console.WriteLine($"Title: {eventCard.Title} - Date: {eventCard.Date} - Location: {eventCard.Location} - Organiser: {eventCard.Organiser}");
         }
+        
+        /* - Testing Finish - */
 
     }
 }
