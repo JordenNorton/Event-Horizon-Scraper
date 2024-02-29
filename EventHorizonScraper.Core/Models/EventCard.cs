d1ca6a79b9f917cef83af4e17b9698d83a5aee04
jordennorton <jordennorton@hotmail.co.uk>
2024-02-29 15:19:58 +0000

    Create event card model

---
+++ EventHorizonScraper.Core/Models/EventCard.cs
@@ -0,0 +1,9 @@
+namespace EventHorizonScraper.Core.Models;
+
+public class EventCard
+{
+    public string? Title { get; set; }
+    public string? Date { get; set; }
+    public string? Location { get; set; }
+    public string? Organiser { get; set; }
+}
